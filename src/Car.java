05876c8338b79f97267c8a2aea4b740d08b3649f
yulia <204513@edu.fa.ru>
2023-09-12 14:47:20 +0300

    initial comit

---
+++ src/Car.java
@@ -0,0 +1,26 @@
+public class Car {
+    private String color;
+    private String brand;
+
+    public Car(String color,
+               String brand) {
+        this.color = color;
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
